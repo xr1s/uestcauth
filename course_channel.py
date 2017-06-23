c219da43b1f1ffabd93005131d3f0085fa92fb22
xris <sir@xris.co>
2017-06-24 00:03:32 +0800

    （伪）错误处理

--- course_channel.py
+++ course_channel.py
@@ -6,12 +6,23 @@ import requests
 from uestcauth import uestc
 from sys import stderr
 from getpass import getpass
-import re
 
 channel_list = range(2000)
 open_channel = []
 
 channel_url = 'http://eams.uestc.edu.cn/eams/stdElectCourse!defaultPage.action?electionProfile.id={channel}'
+
+def valid_channel(chan):
+    response = user.visit(channel_url.format(channel=chan))
+    if response.find('当前用户存在重复登录的情况，已将之前的登录踢出：') != -1:
+        return
+    if response.find('本次会话已经被过期（可能是由于重复登录）') != -1:
+        return
+    if response.find('没有开放的选课轮次') != -1:
+        return 'invalid'
+    if response.find('不在选课时间内') != -1:
+        return 'closed'
+    return 'open'
 
 if __name__ == '__main__':
     stderr.write('Username: ')
@@ -19,11 +30,15 @@ if __name__ == '__main__':
     password = getpass()
     user = uestc(username, password)
     for chan in channel_list:
-        result = user.visit(channel_url.format(channel=chan))
-        if re.search('失败|没有开放', result):
-            print(chan, 'closed')
-        else:
-            open_channel.append(chan)
-            print(chan, 'open')
+        try:
+            status = ''
+            while not status:
+                status = valid_channel(chan)
+                if not status:
+                    print(chan, 'kicked out, retry...')
+        except KeyboardInterrupt:
+            print('keyboard interrupted, exit')
+            break
+        print(chan, status)
     print(open_channel)
 
